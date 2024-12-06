ec4b1cebb8134129486c0bbb33e6af69bd2771f4
root <root@LAPTOP-2JVIQH80.localdomain>
2024-12-06 14:09:42 +0530

    Code updated

--- init_setup.sh
+++ init_setup.sh
@@ -1,9 +1,9 @@
 echo [$(date)]: "START"
-echo [$(date)]: "creating a conda environment python 3.10"
-python3 -m venv env
+echo [$(date)]: "creating a python environment python 3.10"
+python3.10 -m venv env
 echo [$(date)]: "created conda env"
 source env/bin/activate
-echo [$(date)]: "activated conda env"
+echo [$(date)]: "activated python env"
 echo [$(date)]: "installing requirements" 
 pip install -r requirements.txt
 echo [$(date)]: "installed all the requirement"
